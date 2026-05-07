ddb8989255c69465e301e98feaff7f3b95281865
Justin <jj431806@gmail.com>
2026-05-07 15:18:41 -0700

    Basic shop functionality

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,21 +1,61 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
 
 public class Shop : MonoBehaviour
 {
     [Header("UI Elements")] 
-    public GameObject TowerView;
-    public GameObject UpgradeView;
+    public GameObject defaultButton;
+    public TextMeshProUGUI quantityText;
+    
+    [Header("Handlers")]
+    public TowerPlacementManager placementManager;
+    public CoinCounter CoinCounter;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private ShopItem selectedItem;
+
     void Start()
     {
-
+        // Have the shop select a default button
+        SetSelected(defaultButton);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        ShowQuantity();
+    }
 
+    void ShowQuantity()
+    {
+        if (selectedItem.isTower)
+        {
+            // Show the item count
+            quantityText.text = placementManager.TowerCount.ToString("In inventory: 0");
+        }
+    }
+
+    public void SetSelected(GameObject button)
+    {
+        // Select the chosen item
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(button);
+        selectedItem = button.GetComponent<ShopItem>();
+    }
+
+    public void BuyItem()
+    {
+        // Buying a tower
+        // Check if the player has enough coins
+        if (selectedItem.isTower && CoinCounter.GetCoins() >= selectedItem.price)
+        {
+            // Add the tower to the player's count and charge their coins
+            placementManager.AddTower();
+            CoinCounter.RemoveCoins(selectedItem.price);
+        }
+
+        // Keep the item selected
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(selectedItem.gameObject);
     }
 
     public void ExitShop()
